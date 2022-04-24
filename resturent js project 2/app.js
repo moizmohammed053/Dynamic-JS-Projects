2499b21d4ae0763e4536ba2ef9cdc8e8af0c23e2
Mohd.Moiz Ahmed <99491982+moizmohammed053@users.noreply.github.com>
2022-04-24 11:17:47 +0530

    Add files via upload

---
+++ resturent js project 2/app.js
@@ -0,0 +1,73 @@
+let navbar = document.querySelector('.header .navbar');
+let menuBtn = document.querySelector('#menu-btn');
+menuBtn.onclick = () =>{
+  menuBtn.classList.toggle('fa-times');
+  navbar.classList.toggle('active')
+};
+
+var swiper = new Swiper(".home-slider", {
+  grabCursor:true,
+  loop:true,
+  centeredSlides:true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper(".food-slider", {
+  grabCursor:true,
+  loop:true,
+  centeredSlides:true,
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    700: {
+      slidesPerView: 2,
+    },
+    1000: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+var swiper = new Swiper(".menu-slider", {
+  grabCursor:true,
+  loop:true,
+  autoHeight:true,
+  centeredSlides:true,
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+var swiper = new Swiper(".blogs-slider", {
+  grabCursor:true,
+  loop:true,
+  centeredSlides:true,
+  autoHeight:true,
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    700: {
+      slidesPerView: 2,
+    },
+    1000: {
+      slidesPerView: 3,
+    },
+  },
+});
